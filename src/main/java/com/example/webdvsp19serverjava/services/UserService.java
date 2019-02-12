418914cd5e6d692ab1a5a0c767deb16f6259cf70
Anurag Kumar <kumar.anur@husky.neu.edu>
2019-02-12 18:31:53 -0500

    added dummy objects for initial testing

--- src/main/java/com/example/webdvsp19serverjava/services/UserService.java
+++ src/main/java/com/example/webdvsp19serverjava/services/UserService.java
@@ -20,6 +20,7 @@ import com.example.webdvsp19serverjava.models.ParagraphWidget;
 import com.example.webdvsp19serverjava.models.Sections;
 import com.example.webdvsp19serverjava.models.Students;
 import com.example.webdvsp19serverjava.models.Topics;
+import com.example.webdvsp19serverjava.models.Widgets;
 
 import java.util.ArrayList;
 
@@ -33,6 +34,7 @@ public class UserService {
 	
 	Courses cs5610 = new Courses(1, "Web Dev");
 	Courses cs5200 = new Courses(2, "Database");
+	Courses cs5600 = new Courses(3, "Software Engineering");
 	
 	Sections section1 = new Sections(1, "Section 1");
 	Sections section2 = new Sections(2, "Section 2");
@@ -41,22 +43,107 @@ public class UserService {
 	Modules react = new Modules(2, "React");
 	Modules sql = new Modules(1, "SQL");
 	Modules jpa = new Modules(2, "JPA");
+	Modules agile = new Modules(1, "Agile");
+	Modules testing = new Modules(2, "Testing");
 	
-	Chapters chapter1 = new Chapters(1, "Lesson 1");
-	Chapters chapter2 = new Chapters(2, "Lesson 2");
-	Chapters chapter3 = new Chapters(1, "Lesson 1");
-	Chapters chapter4 = new Chapters(2, "Lesson 2");
+	Chapters chapter1 = new Chapters(1, "Lesson 1 Jquery Basics");
+	Chapters chapter2 = new Chapters(2, "Lesson 2 Jquery Syntax");
+	Chapters chapter3 = new Chapters(1, "Lesson 1 React State");
+	Chapters chapter4 = new Chapters(2, "Lesson 2 React Render");
+	Chapters chapter5 = new Chapters(1, "Lesson 1 SQL Syntax");
+	Chapters chapter6 = new Chapters(2, "Lesson 2 SQL Keys");
+	Chapters chapter7 = new Chapters(1, "Lesson 1 JPA API");
+	Chapters chapter8 = new Chapters(2, "Lesson 2 JPA Arch");
+	Chapters chapter9 = new Chapters(1, "Lesson 1 Agile Methods");
+	Chapters chapter10 = new Chapters(2, "Lesson 2 Agile Workflow");
+	Chapters chapter11 = new Chapters(1, "Lesson 1 Unit Testing");
+	Chapters chapter12 = new Chapters(2, "Lesson 2 Regression Testing");
 	
 	Topics topic1 = new Topics(1, "Topic 1");
 	Topics topic2 = new Topics(2, "Topic 2");
 	Topics topic3 = new Topics(1, "Topic 1");
 	Topics topic4 = new Topics(2, "Topic 2");
+	Topics topic5 = new Topics(1, "Topic 1");
+	Topics topic6 = new Topics(2, "Topic 2");
 	
 	HeadingWidget heading1 = new HeadingWidget(1, "Heading Text");
 	ParagraphWidget paragraph1 = new ParagraphWidget(1, "Paragraph Text");
 	ImageWidget image1 = new ImageWidget(1, "Image URL");
 	ListWidget list1 = new ListWidget(1, "List Text");
 	LinkWidget link1 = new LinkWidget(1, "Link URL");
+	
+	ArrayList<Courses> courses1 = new ArrayList<Courses>();
+	ArrayList<Courses> courses2 = new ArrayList<Courses>();
+	ArrayList<Modules> cs5610Modules = new ArrayList<Modules>();
+	ArrayList<Modules> cs5200Modules = new ArrayList<Modules>();
+	ArrayList<Modules> cs5600Modules = new ArrayList<Modules>();
+	ArrayList<Chapters> jqueryChapters = new ArrayList<Chapters>();
+	ArrayList<Chapters> reactChapters = new ArrayList<Chapters>();
+	ArrayList<Chapters> sqlChapters = new ArrayList<Chapters>();
+	ArrayList<Chapters> jpaChapters = new ArrayList<Chapters>();
+	ArrayList<Chapters> agileChapters = new ArrayList<Chapters>();
+	ArrayList<Chapters> testingChapters = new ArrayList<Chapters>();
+	ArrayList<Topics> jqueryLessTopics = new ArrayList<Topics>();
+	ArrayList<Topics> sqlLessTopics = new ArrayList<Topics>();
+	ArrayList<Widgets> jquerytopic1 = new ArrayList<Widgets>();
+	ArrayList<Widgets> jquerytopic2 = new ArrayList<Widgets>();
+	ArrayList<Widgets> sqltopic1 = new ArrayList<Widgets>();
+	ArrayList<Widgets> sqltopic2 = new ArrayList<Widgets>();
+	{
+		sqltopic2.add(image1);
+		sqltopic2.add(list1);
+		topic4.setWidgets(sqltopic2);
+		sqltopic1.add(heading1);
+		sqltopic1.add(image1);
+		topic3.setWidgets(sqltopic1);
+		jquerytopic2.add(heading1);
+		jquerytopic2.add(image1);
+		jquerytopic2.add(paragraph1);
+		jquerytopic2.add(list1);
+		jquerytopic2.add(link1);
+		topic2.setWidgets(jquerytopic2);
+		jquerytopic1.add(heading1);
+		jquerytopic1.add(paragraph1);
+		topic1.setWidgets(jquerytopic1);
+		sqlLessTopics.add(topic3);
+		sqlLessTopics.add(topic4);
+		chapter3.setTopics(sqlLessTopics);
+		jqueryLessTopics.add(topic1);
+		jqueryLessTopics.add(topic2);
+		chapter1.setTopics(jqueryLessTopics);
+		testingChapters.add(chapter11);
+		testingChapters.add(chapter12);
+		testing.setChapters(testingChapters);
+		agileChapters.add(chapter9);
+		agileChapters.add(chapter10);
+		agile.setChapters(agileChapters);
+		jpaChapters.add(chapter7);
+		jpaChapters.add(chapter8);
+		jpa.setChapters(jpaChapters);
+		sqlChapters.add(chapter5);
+		sqlChapters.add(chapter6);
+		sql.setChapters(sqlChapters);
+		reactChapters.add(chapter3);
+		reactChapters.add(chapter4);
+		react.setChapters(reactChapters);
+		jqueryChapters.add(chapter1);
+		jqueryChapters.add(chapter2);
+		jquery.setChapters(jqueryChapters);
+		cs5610Modules.add(jquery);
+		cs5610Modules.add(react);
+		cs5200Modules.add(jpa);
+		cs5200Modules.add(sql);
+		cs5600Modules.add(agile);
+		cs5600Modules.add(testing);
+		cs5610.setModules(cs5610Modules);
+		cs5200.setModules(cs5200Modules);
+		cs5600.setModules(cs5600Modules);
+		courses1.add(cs5610);
+		courses1.add(cs5610);
+		courses2.add(cs5600);
+		alex.setCourses(courses1);
+		alice.setCourses(courses2);
+	}
 //	Person alice = new Person(100, "bird", "bird", "Alice", "Kathie", "Faculty");
 //	Person alex = new Person(101, "sign", "sign", "Alex", "Mercer", "Student");
 //	Courses cs5610 = new Courses(100, "cs5610");
