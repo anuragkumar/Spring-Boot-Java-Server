319cdd798bb5f1a657fe75dcbbe72e96632e497b
Anurag Kumar <kumar.anur@husky.neu.edu>
2019-02-13 00:22:11 -0500

    added lesson services

--- src/main/java/com/example/webdvsp19serverjava/models/Chapters.java
+++ src/main/java/com/example/webdvsp19serverjava/models/Chapters.java
@@ -29,5 +29,38 @@ public class Chapters {
 	public void setTopics(ArrayList<Topics> topics) {
 		this.topics = topics;
 	}
+	public ArrayList<Topics> createTopic(Topics topic) {
+		this.topics.add(topic);
+		return this.topics;
+	}
+	public ArrayList<Topics> findAllTopics() {
+		return this.topics;
+	}
+	public Topics findTopicById(Integer tid) {
+		for(Topics topic: this.topics) {
+			if(topic.getId() == tid) {
+				return topic;
+			}
+		}
+		return null;
+	}
+	public ArrayList<Topics> updateTopic(Integer tid, Topics topic) {
+		for(Topics top: this.topics) {
+			if(top.getId() == id) {
+				top.setTopicName(topic.getTopicName());
+				return this.topics;
+			}
+		}
+		return null;
+	}
+	public ArrayList<Topics> deleteTopic(Integer tid) {
+		for(Topics topic: this.topics) {
+			if(topic.getId() == id) {
+				this.topics.remove(topic);
+				return this.topics;
+			}
+		}
+		return null;
+	}
 	
 }
--- src/main/java/com/example/webdvsp19serverjava/services/TopicService.java
+++ src/main/java/com/example/webdvsp19serverjava/services/TopicService.java
@@ -16,61 +16,72 @@ import com.example.webdvsp19serverjava.models.Chapters;
 import com.example.webdvsp19serverjava.models.Courses;
 import com.example.webdvsp19serverjava.models.Faculty;
 import com.example.webdvsp19serverjava.models.Modules;
+import com.example.webdvsp19serverjava.models.Topics;
 
 @RestController
-public class LessonService {
-	@PostMapping("/api/courses/{cid}/{mid}/lessons")
-	public ArrayList<Chapters> createLesson(@PathVariable("cid") Integer cid,
+public class TopicService {
+	@PostMapping("/api/courses/{cid}/{mid}/lessons/{lid}/topics")
+	public ArrayList<Topics> createTopic(@PathVariable("cid") Integer cid,
 											@PathVariable("mid") Integer mid,
-											@RequestBody Chapters chapter,
+											@PathVariable("lid") Integer lid,
+											@RequestBody Topics topic,
 											HttpSession session){
 		Faculty user = (Faculty)session.getAttribute("currentUser");
 		Courses course = user.findCourseById(cid);
 		Modules module = course.findModuleById(mid);
-		return module.createLesson(chapter);
+		Chapters chapter = module.findLessonById(lid);
+		return chapter.createTopic(topic);
 	}
 	
-	@GetMapping("/api/course/{cid}/{mid}/lessons")
-	public ArrayList<Chapters> findAllLessons(@PathVariable("cid") Integer cid, 
-											@PathVariable("mid") Integer mid,
-											HttpSession session){
-		Faculty user = (Faculty)session.getAttribute("currentUser");
-		Courses course = user.findCourseById(cid);
-		Modules module = course.findModuleById(mid);
-		return module.findAllLessons();
-	}
-	
-	@GetMapping("/api/modules/{cid}/{mid}/lessons/{lid}")
-	public Chapters findLessonById(@PathVariable("cid") Integer cid, 
-									@PathVariable("mid") Integer mid, 
-									@PathVariable("lid") Integer lid,
-									HttpSession session){
-		Faculty user = (Faculty)session.getAttribute("currentUser");
-		Courses course = user.findCourseById(cid);
-		Modules module = course.findModuleById(mid);
-		return module.findLessonById(lid);
-	}
-	
-	@PutMapping("/api/modules/{cid}/{mid}/lessons/{lid}")
-	public ArrayList<Chapters> updateLesson(@PathVariable("cid") Integer cid, 
-									@PathVariable("mid") Integer mid, 
-									@PathVariable("lid") Integer lid, 
-									@RequestBody Chapters chapter,
-									HttpSession session){
-		Faculty user = (Faculty)session.getAttribute("currentUser");
-		Courses course = user.findCourseById(cid);
-		Modules module = course.findModuleById(mid);
-		return module.updateLesson(lid, chapter);
-	}
-	
-	@DeleteMapping("/api/courses/{cid}/{mid}/lessons/{lid}")
-	public ArrayList<Chapters> deleteCourse(@PathVariable("cid") Integer cid, 
+	@GetMapping("/api/course/{cid}/{mid}/lessons/{lid}/topics")
+	public ArrayList<Topics> findAllTopics(@PathVariable("cid") Integer cid, 
 											@PathVariable("mid") Integer mid,
 											@PathVariable("lid") Integer lid,
 											HttpSession session){
 		Faculty user = (Faculty)session.getAttribute("currentUser");
 		Courses course = user.findCourseById(cid);
 		Modules module = course.findModuleById(mid);
-		return module.deleteLesson(lid);
+		Chapters chapter = module.findLessonById(lid);
+		return chapter.findAllTopics();
+	}
+	
+	@GetMapping("/api/modules/{cid}/{mid}/lessons/{lid}/topics/{tid}")
+	public Topics findLessonById(@PathVariable("cid") Integer cid, 
+									@PathVariable("mid") Integer mid, 
+									@PathVariable("lid") Integer lid,
+									@PathVariable("tid") Integer tid,
+									HttpSession session){
+		Faculty user = (Faculty)session.getAttribute("currentUser");
+		Courses course = user.findCourseById(cid);
+		Modules module = course.findModuleById(mid);
+		Chapters chapter = module.findLessonById(lid);
+		return chapter.findTopicById(tid);
+	}
+	
+	@PutMapping("/api/modules/{cid}/{mid}/lessons/{lid}/topics/{tid}")
+	public ArrayList<Topics> updateTopic(@PathVariable("cid") Integer cid, 
+									@PathVariable("mid") Integer mid, 
+									@PathVariable("lid") Integer lid, 
+									@PathVariable("tid") Integer tid, 
+									@RequestBody Topics topic,
+									HttpSession session){
+		Faculty user = (Faculty)session.getAttribute("currentUser");
+		Courses course = user.findCourseById(cid);
+		Modules module = course.findModuleById(mid);
+		Chapters chapter = module.findLessonById(lid);
+		return chapter.updateTopic(tid, topic);
+	}
+	
+	@DeleteMapping("/api/courses/{cid}/{mid}/lessons/{lid}/topics/{tid}")
+	public ArrayList<Topics> deleteTopic(@PathVariable("cid") Integer cid, 
+											@PathVariable("mid") Integer mid,
+											@PathVariable("lid") Integer lid,
+											@PathVariable("tid") Integer tid,
+											HttpSession session){
+		Faculty user = (Faculty)session.getAttribute("currentUser");
+		Courses course = user.findCourseById(cid);
+		Modules module = course.findModuleById(mid);
+		Chapters chapter = module.findLessonById(lid);
+		return chapter.deleteTopic(tid);
 	}
 }
