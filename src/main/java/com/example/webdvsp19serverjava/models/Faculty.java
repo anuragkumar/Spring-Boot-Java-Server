5e63650828c5a865d05b95868075a24532376e9e
Anurag Kumar <kumar.anur@husky.neu.edu>
2019-03-14 21:26:27 -0400

    modified faculty class with jpa

--- src/main/java/com/example/webdvsp19serverjava/models/Faculty.java
+++ src/main/java/com/example/webdvsp19serverjava/models/Faculty.java
@@ -2,15 +2,64 @@ package com.example.webdvsp19serverjava.models;
 
 import java.util.ArrayList;
 
-public class Faculty extends Person {
-	public Faculty(int id, String username, String password, String firstName, String lastName){
-		super(id, username, password, firstName, lastName);
-		courses = new ArrayList<Courses>();
-		sections = new ArrayList<Sections>();
-	}
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Faculty {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    @OneToMany(mappedBy = "author")
+    private ArrayList<Courses> courses;
+
+    public Faculty() {}
+    
+    public Faculty(int id, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 	
-	ArrayList<Courses> courses;
-	ArrayList<Sections> sections;
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
 	public ArrayList<Courses> getCourses() {
 		return courses;
