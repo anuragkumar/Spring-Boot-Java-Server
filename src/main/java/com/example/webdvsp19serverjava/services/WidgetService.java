affe5ba434f18f8b5f550c1eb4e6923e3d156950
Anurag Kumar <kumar.anur@husky.neu.edu>
2019-03-15 01:23:56 -0400

    updated widget service

--- src/main/java/com/example/webdvsp19serverjava/services/WidgetService.java
+++ src/main/java/com/example/webdvsp19serverjava/services/WidgetService.java
@@ -2,67 +2,80 @@ package com.example.webdvsp19serverjava.services;
 
 import java.util.ArrayList;
 
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.webdvsp19serverjava.models.Chapters;
-import com.example.webdvsp19serverjava.models.Courses;
-import com.example.webdvsp19serverjava.models.Faculty;
-import com.example.webdvsp19serverjava.models.Modules;
 import com.example.webdvsp19serverjava.models.Topics;
 import com.example.webdvsp19serverjava.models.Widgets;
+import com.example.webdvsp19serverjava.repositories.TopicRepository;
+import com.example.webdvsp19serverjava.repositories.WidgetRepository;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials ="true")
+@CrossOrigin(origins = "*", allowCredentials ="true", allowedHeaders = "*")
 public class WidgetService {
-	@PostMapping("/api/courses/{cid}/modules/{mid}/lessons/{lid}/topics/{tid}/widgets")
-	public ArrayList<Widgets> createTopic(@PathVariable("cid") Integer cid,
-											@PathVariable("mid") Integer mid,
-											@PathVariable("lid") Integer lid,
-											@PathVariable("tid") Integer tid,
-											@RequestBody Widgets widget,
-											HttpSession session){
-		Faculty user = (Faculty)session.getAttribute("currentUser");
-		Courses course = user.findCourseById(cid);
-		Modules module = course.findModuleById(mid);
-		Chapters chapter = module.findLessonById(lid);
-		Topics topic = chapter.findTopicById(tid);
-		return topic.createWidget(widget);
-	}
-	
-	@GetMapping("/api/courses/{cid}/modules/{mid}/lessons/{lid}/topics/{tid}/widgets")
-	public ArrayList<Widgets> findAllWidgets(@PathVariable("cid") Integer cid, 
-											@PathVariable("mid") Integer mid,
-											@PathVariable("lid") Integer lid,
-											@PathVariable("tid") Integer tid,
-											HttpSession session){
-		Faculty user = (Faculty)session.getAttribute("currentUser");
-		Courses course = user.findCourseById(cid);
-		Modules module = course.findModuleById(mid);
-		Chapters chapter = module.findLessonById(lid);
-		Topics topic = chapter.findTopicById(tid);
-		return topic.findAllWidgets();
-	}
-	
-	@DeleteMapping("/api/courses/{cid}/modules/{mid}/lessons/{lid}/topics/{tid}/widgets/{wid}")
-	public ArrayList<Widgets> deleteTopic(@PathVariable("cid") Integer cid, 
-											@PathVariable("mid") Integer mid,
-											@PathVariable("lid") Integer lid,
-											@PathVariable("tid") Integer tid,
-											@PathVariable("wid") Integer wid,
-											HttpSession session){
-		Faculty user = (Faculty)session.getAttribute("currentUser");
-		Courses course = user.findCourseById(cid);
-		Modules module = course.findModuleById(mid);
-		Chapters chapter = module.findLessonById(lid);
-		Topics topic = chapter.findTopicById(tid);
-		return topic.deleteWidget(wid);
-	}
+	@Autowired
+    TopicRepository topicRepositroy;
+    @Autowired
+    WidgetRepository widgetRepository;
+    
+    @GetMapping("/api/topic/{tid}/widgets")
+    public ArrayList<Widgets> findAllWidgets(@PathVariable("tid") int topicId,
+            								HttpSession session) {
+        Topics topic = topicRepositroy.findById(topicId).get();
+        return topic.getWidgets();
+    }
+
+    @PostMapping("/api/topic/{tid}/widgets")
+    public ArrayList<Widgets> createWidget(@PathVariable("tid") int topicId,
+            								@RequestBody Widgets newWidget,
+            								HttpSession session) {
+        Topics topic = topicRepositroy.findById(topicId).get();
+        newWidget.setTopic(topic);
+        widgetRepository.save(newWidget);
+        return topicRepositroy.findById(topicId).get().getWidgets();
+    }
+
+    @GetMapping("/api/widget/{wid}")
+    public Widgets findWidgetById(@PathVariable("wid") int widgetId,
+            					HttpSession session) {
+        Widgets widget = widgetRepository.findById(widgetId).get();
+        if(widget !=null) {
+            return widget;
+        }
+
+        return new Widgets();
+    }
+
+    @PutMapping("/api/widget/{wid}")
+    public ArrayList<Widgets> updateWidget(@PathVariable("wid") int widgetId,
+            								@RequestBody Widgets widgetToBeUpdated,
+            								HttpSession session) {
+        return null;
+    }
+
+    @DeleteMapping("/api/widget/{wid}")
+    public ArrayList<Widgets> deleteWidget(@PathVariable("wid") int widgetId,
+            								HttpSession session) {
+        widgetRepository.deleteById(widgetId);
+        return null;
+    }
+
+    @DeleteMapping("/api/widget/delete-all/{tid}")
+    public void deleteAllWidgets(@PathVariable("tid") int topicId,
+            					HttpSession session) {
+        Topics topic = topicRepositroy.findById(topicId).get();
+        ArrayList<Widgets> widgets = topic.getWidgets();
+        for(Widgets widget: widgets) {
+            widgetRepository.delete(widget);
+        }
+    }
 }
